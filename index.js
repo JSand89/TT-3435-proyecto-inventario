af643da8aa0cf1604e4a8eb130bddd043eb518db
JSand89 <javier.esand@gmail.com>
2024-12-11 20:15:29 -0500

    Realizando rutas y controladores

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-//trae express para usarlo
 const express = require("express");
-
-//me crea una variable para usar express
+const holaRoutes = require("./routes/holaRoutes")
 const app = express();
-
-//definimos el puerto
 PORT = 3000;
-
-//configuramos el puerto
 app.set("port",PORT);
-
-//definimos una ruta y un verbo HTTP, (req,res) creamos una función que tiene como parametros req:request y res:response el request es lo que recibimos el response es lo que respondemos 
 app.get("/hola",(req,res)=>{
     res.send("hola mundo")
 })
 
+// Routes
+app.use("/api/hola",holaRoutes)
 app.listen(PORT,()=>{
     console.log(`Listening port ${PORT}`)
 })
