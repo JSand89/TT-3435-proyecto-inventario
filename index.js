46ce2cab634b5088a7998b913d52b3e90d146746
JSand89 <javier.esand@gmail.com>
2024-12-13 21:17:13 -0500

    post y get de inventario

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const holaRoutes = require("./routes/holaRoutes")
+const mongoose = require("mongoose");
+require("dotenv").config()
+const holaRoutes = require("./routes/holaRoutes");
+const inventarioRoutes = require("./routes/inventario");
 const app = express();
 PORT = 3000;
 app.set("port",PORT);
@@ -7,8 +10,16 @@ app.get("/hola",(req,res)=>{
     res.send("hola mundo")
 })
 
+app.use(express.json())
+
 // Routes
-app.use("/api/hola",holaRoutes)
+app.use("/api/hola",holaRoutes);
+app.use("/api/inventario",inventarioRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("Conectado a la Base de datos"))
+.catch((err)=>console.error(err))
+
 app.listen(PORT,()=>{
     console.log(`Listening port ${PORT}`)
 })
---
+++ routes/inventario.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const {obtenerInventario, crearHerramienta} = require("../controllers/inventario")
+const router = express.Router();
+
+router.get("/",obtenerInventario)
+router.post("/", crearHerramienta)
+module.exports = router;
